8322e67cb25a0392f7c4db6b0dafe0830bc134fd
sworley8 <sophiaworley@hotmail.com>
2020-02-23 01:41:01 -0500

    finished settings and worked on experimental stuff for dialogue

---
+++ RedHerringGame/Assets/Scripts/DialogueStuff/experimentWithDialogue/NewDialogueSystem.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class NewDialogueSystem : MonoBehaviour
+{
+	public static NewDialogueSystem instance;
+
+	public ELEMENTS elements;
+
+	void Awake()
+	{
+		instance = this;
+	}
+
+	/// <summary>
+	/// Say something and show it on the speech box.
+	/// </summary>
+	public void Say(string speech, string speaker = "")
+	{
+		StopSpeaking();
+
+		speaking = StartCoroutine(Speaking(speech, false, speaker));
+	}
+
+	/// <summary>
+	/// Say something to be added to what is already on the speech box.
+	/// </summary>
+	public void SayAdd(string speech, string speaker = "")
+	{
+		StopSpeaking();
+
+		speechText.text = targetSpeech;
+
+		speaking = StartCoroutine(Speaking(speech, true, speaker));
+	}
+
+	public void StopSpeaking()
+	{
+		if (isSpeaking)
+		{
+			StopCoroutine(speaking);
+		}
+		if (textArchitect != null && textArchitect.isConstructing)
+		{
+			textArchitect.Stop();
+		}
+		speaking = null;
+	}
+
+	public bool isSpeaking { get { return speaking != null; } }
+	[HideInInspector] public bool isWaitingForUserInput = false;
+
+	public string targetSpeech = "";
+	Coroutine speaking = null;
+	TextArchitect textArchitect = null;
+
+	IEnumerator Speaking(string speech, bool additive, string speaker = "")
+	{
+		speechPanel.SetActive(true);
+
+		string additiveSpeech = additive ? speechText.text : "";
+		targetSpeech = additiveSpeech + speech;
+
+		textArchitect = new TextArchitect(speech, additiveSpeech);
+
+		speakerNameText.text = DetermineSpeaker(speaker);//temporary
+
+		isWaitingForUserInput = false;
+
+		while (textArchitect.isConstructing)
+		{
+			if (Input.GetKey(KeyCode.Space))
+				textArchitect.skip = true;
+
+			speechText.text = textArchitect.currentText;
+
+			yield return new WaitForEndOfFrame();
+		}
+		//if skipping stopped the display text from updating correctly, force it to update at the end.
+		speechText.text = textArchitect.currentText;
+
+		//text finished
+		isWaitingForUserInput = true;
+		while (isWaitingForUserInput)
+			yield return new WaitForEndOfFrame();
+
+		StopSpeaking();
+	}
+
+	string DetermineSpeaker(string s)
+	{
+		string retVal = speakerNameText.text;//default return is the current name
+		if (s != speakerNameText.text && s != "")
+			retVal = (s.ToLower().Contains("narrator")) ? "" : s;
+
+		return retVal;
+	}
+
+	/// <summary>
+	/// Close the entire speech panel. Stop all dialogue.
+	/// </summary>
+	public void Close()
+	{
+		StopSpeaking();
+		speechPanel.SetActive(false);
+	}
+
+	[System.Serializable]
+	public class ELEMENTS
+	{
+		/// <summary>
+		/// The main panel containing all dialogue related elements on the UI
+		/// </summary>
+		public GameObject speechPanel;
+		public TextMeshProUGUI speakerNameText;
+		public TextMeshProUGUI speechText;
+	}
+	public GameObject speechPanel { get { return elements.speechPanel; } }
+	public TextMeshProUGUI speakerNameText { get { return elements.speakerNameText; } }
+	public TextMeshProUGUI speechText { get { return elements.speechText; } }
+}
---
+++ RedHerringGame/Assets/Scripts/DialogueStuff/experimentWithDialogue/TestArchitect.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TestArchitect : MonoBehaviour
+{
+    public Text text;
+    TextArchitect architect;
+    [TextArea(5, 10)]
+    public string say;
+    public int characterPerFrame = 1;
+    public float speed = 1f;
+    public bool useEncap = true;
+    // Start is called before the first frame update
+    void Start()
+    {
+        architect = new TextArchitect(say);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            architect = new TextArchitect(say, "", characterPerFrame, speed, useEncap);
+        }
+        text.text = architect.currentText;
+    }
+}
