84525a5b3c915ac79905a7374f3162b743087985
sworley8 <sophiaworley@hotmail.com>
2020-02-26 14:55:18 -0500

    gender screen

---
+++ RedHerringGame/Assets/Scripts/PronounAndAvatar.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PronounAndAvatar : MonoBehaviour
+{
+    public string pronoun;
+    public int avatar;
+    public GameObject playerchoice;
+    void Awake()
+    {
+        DontDestroyOnLoad(playerchoice);
+    }
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    //// Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+}
